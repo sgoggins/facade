178917ec8b4ae95ac3a883fc3b34a6caa6ee2294
Sean Goggins <s@goggins.com>
2019-03-18 00:45:21 +0100

    added mariadb config script

---
+++ utilities/install_deps-mariadb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "
+This script will install the necessary dependencies to run Facade in headless
+mode. This means that it will not install Apache or the required PHP packages.
+You must use the CLI to configure Facade and export analysis data, which is
+available in cli/facade.py
+
+You can always convert to a full installation by running the full script to fill
+in any missing packages, should you change your mind later:
+ $ ./install_deps-deb-full.sh
+
+Installing any missing dependencies...
+"
+
+sudo apt-get install mysql-client mariadb-server \
+python3 python3-mysqldb python3-bcrypt python3-xlsxwriter python3-texttable
+
+echo "
+If everything went well, your next step is to run setup:
+ $ ./setup.py
+ "
+
