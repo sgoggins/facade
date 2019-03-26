2bd2750ca2b3baf623bae8511b8567a54ab84b10
Sean Goggins <s@goggins.com>
2019-03-26 21:23:54 +0100

    update

--- utilities/reset-status.py
+++ utilities/reset-status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright 2016-2018 Brian Warner
+# Updated in sgoggins/facade branch
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
